638b243bae824a8a2347bc6013fc644532bd4b9c
mcpolandc <mcpolandc@gmail.com>
2022-05-09 15:23:22 -0500

    fix linter

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-const Logo = () => {
+const Logo = (): ReactElement => {
   return <img src="/assets/images/logo.svg" />;
 };
 
